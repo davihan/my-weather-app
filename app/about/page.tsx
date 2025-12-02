a5c4a801b4826bb99f22d1692965e93051399f8c
davihan <david.hanych@gmail.com>
2025-12-02 23:39:27 +0100

    par added in vscode

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,6 +4,7 @@ export default function About() {
         <div>
             <h1 className="text-2xl text-blue-800">About this web</h1>
             <p>This weather app ...</p>
+            <p>This is new paragraph added in VS Code ...</p>
         </div>
     );
 }
